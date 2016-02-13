94bd44bfcb344130921503127b8d49b1b00b1101
Bradley Bartholomew <bradley.bartholomew@live.com>
2016-02-13 14:13:49 -0800

    Removing dependency on bash and adding control for branch

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 var express = require('express'); 
-var spawn = require('child_process').spawn; 
+var exec = require('child_process').exec; 
+var bodyParser = require('body-parser'); 
 
 module.exports = function (config) {
-    return express.Router().post('/deployments/:repo', deploymentHandler)
-        .get('/deployments/:repo', deploymentHandler);
-
-    function deploymentHandler(request, response) {
-        if(!config || !config[request.params.repo]) {
-            response.status(404).send('Not found'); 
-            return; 
-        }
-        console.log('Detected update for respository \'', request.params.repo, '\'')
-        var cmd = spawn(__dirname + '/deploy.sh', [ request.params.repo ]);
-        cmd.stdout.on('data', function (data) {
-            console.log(data.toString()); 
-        }) 
-        cmd.stderr.on('data', function (data) {
-            console.log(data.toString()); 
-        }) 
-        response.send(''); 
+    return express.Router()
+        .use(bodyParser.json()) 
+        .post('/deployments/:repo', deploymentsRouter); 
+        
+    function deploymentsRouter(req, res) {
+        deploymentHandler(config, req, res); 
     }
 }; 
 
+function deploymentHandler(config, request, response) {
+    var repo = request.params.repo; 
+    var branch = request.body.ref && request.body.ref.replace('refs/heads/', ''); 
+    
+    var info = config && config[repo] && config[repo][branch]; 
+    
+    if(!info) {
+        response.status(404).send('Not found'); 
+        return; 
+    }
+    
+    if(!info.path) {
+        response.send(''); 
+        return; 
+    }
+    
+    var cmd = "git pull && npm install && npm test"; 
+    cmd += info.restartCommand ? " && " + info.restartCommand : ""; 
+    exec(cmd, { cwd: info.path }, function (error, stdout, stderr) {
+        if(error) {
+            console.log(stdout); 
+            console.log(stderr); 
+            response.status(500).send('Error occurred during deployment');
+            return; 
+        } else {
+            console.log('Deployment success for ', request.params.repo, '/', request.body.ref); 
+            response.send(''); 
+            return;             
+        }
+    }); 
+}
